2a98bc37d71f347260ae4182df3345e5b23061c3
Hakan Türkmen <hakannturkmen@gmail.com>
2025-06-18 14:13:41 +0300

    Refactor: externalize Firebase config and add example file

---
+++ firebaseConfig.example.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "your-project.firebaseapp.com",
+  projectId: "your-project",
+  storageBucket: "your-project.appspot.com",
+  messagingSenderId: "000000000000",
+  appId: "1:000000:web:abcdef",
+  measurementId: "G-XXXXXXX",
+  databaseURL: "https://your-project-default-rtdb.region.firebasedatabase.app"
+}; 
