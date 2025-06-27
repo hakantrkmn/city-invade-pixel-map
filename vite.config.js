104ed01adf6e267c5f16673eaa3b1a4662585408
Hakan Türkmen <hakannturkmen@gmail.com>
2025-06-27 21:45:55 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite';
 
 // Allow overriding base path via CI env (e.g., BASE_PATH=/city-invade-pixel-map/usa/)
 export default defineConfig({
-  base: process.env.BASE_PATH || '/city-invade-pixel-map/'
+  base: process.env.BASE_PATH || '/city-invade-pixel-map/',
+  build: {
+    target: 'esnext'
+  }
 }); 
